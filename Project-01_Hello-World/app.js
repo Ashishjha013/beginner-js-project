357fd475d50233c8818941d04c1b16b6229a121a
Ashish Kumar Jha <ashishjha1304@gmail.com>
2025-08-02 12:17:06 +0530

    Update app.js

--- Project-01_Hello-World/app.js
+++ Project-01_Hello-World/app.js
@@ -2,6 +2,12 @@ const btnElement = document.querySelector("button");
 const spanElement = document.querySelector("span");
 
 btnElement.addEventListener("click", () => {
-  const yourName = prompt("Please enter your name : ");
-  spanElement.textContent = yourName;
+  const yourName = prompt("What’s your name?");
+  if (yourName?.trim()) {
+    spanElement.textContent = yourName.trim();
+    spanElement.style.color = "#7a00e3";
+  } else {
+    spanElement.textContent = "World";
+    spanElement.style.color = "red";
+  }
 });
